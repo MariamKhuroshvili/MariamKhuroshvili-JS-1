a72ca0ecbe0939d93a9ff10506a44e6a073a179e
MariamKhuroshvili <mariam.khuroshvili97@gmail.com>
2023-05-03 19:13:57 +0400

    add script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let number = 100;
+if ( number <50 ) {
+    console.log ('50')
+}
+else if ( number > 20 ) {
+    console.log ('>20')
+}
+else {
+        console.log ('error')
+}
+ 
+
+let userName = 'Mariam';
+if ( userName == 'Mariam') {
+    console.log ('true')
+}
+else {
+    console.log ('false')
+}
+
+let name = 'Mariam'
+switch (userName) {
+    case 'Mariam' : 
+        console.log ('true');
+        break;
+        default: 
+            console.log ('false');
+}
